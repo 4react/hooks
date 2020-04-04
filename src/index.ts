e94210a30288ffd33d1f4eb88dd32f647e4d0522
zanomate-desktop <zanomate@gmail.com>
2020-04-04 13:49:59 +0200

    change build folder structure

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import useArray from './useArray'
+import useDocumentSize from './useDocumentSize'
+import useElementSize from './useElementSize'
+import useInterval from './useInterval'
+import useScreenOrientation from './useScreenOrientation'
+import useTimeout from './useTimeout'
+import useWindowScroll from './useWindowScroll'
+
+export default {
+  useArray,
+  useDocumentSize,
+  useElementSize,
+  useInterval,
+  useScreenOrientation,
+  useTimeout,
+  useWindowScroll
+}
